22a99868e4ca92c53cc86c34dc0f0615a718eee2
JuliaGrisales <jgrisales@gmail.com>
2020-10-27 14:10:12 +0100

    ExpenseReport model

---
+++ database/factories/ExpenseReportFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ExpenseReport;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ExpenseReportFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = ExpenseReport::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
