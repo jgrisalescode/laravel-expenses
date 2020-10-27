15ad056c1614efd93f2a86d598fb83601dc3face
JuliaGrisales <jgrisales@gmail.com>
2020-10-27 21:51:52 +0100

    latouts.blade

--- resources/views/expenses/index.blade.php
+++ resources/views/expenses/index.blade.php
@@ -1,34 +1,18 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <title>Document</title>
-</head>
-<body>
-<div class="container">
-    <div class="row">
-        <div class="col">
-            <h1>Expenses</h1>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>Detalle</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($expenses as $expense)
-                        <tr>
-                            <td>{{$expense->title}}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-    </div>
-</div>
-</body>
-</html>
+@extends('layouts.base')
+@section('content')
+    <h1>Reports</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Detalle</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($expenses as $expense)
+                <tr>
+                    <td>{{$expense->title}}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
